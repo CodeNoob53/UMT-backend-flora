94b70a14eb1dcd50a2810dd22b06bf350b2c6588
Code_NooB <alexangel77719@gmail.com>
2026-06-23 00:22:16 +0300

    feat: add authentication requirement to bouquet routes and update Swagger documentation

--- routes/api/bouquetRouter.js
+++ routes/api/bouquetRouter.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import * as ctrl from '../../controllers/bouquetController.js';
+import { basicAuth } from '../../middlewares/basicAuth.js';
 import { validateBody } from '../../middlewares/validateBody.js';
 import { upload } from '../../middlewares/multerUpload.js';
 import { bouquetCreateSchema, bouquetUpdateSchema } from '../../schemas/bouquetSchema.js';
@@ -8,10 +9,10 @@ const router = Router();
 
 router.get('/', ctrl.getAll);
 router.get('/:id', ctrl.getById);
-router.post('/', validateBody(bouquetCreateSchema), ctrl.create);
-router.put('/:id', validateBody(bouquetUpdateSchema), ctrl.update);
-router.delete('/:id', ctrl.remove);
-router.patch('/:id/favorite', ctrl.toggleFavorite);
-router.patch('/:id/photo', upload.single('photo'), ctrl.updatePhoto);
+router.post('/', basicAuth, validateBody(bouquetCreateSchema), ctrl.create);
+router.put('/:id', basicAuth, validateBody(bouquetUpdateSchema), ctrl.update);
+router.delete('/:id', basicAuth, ctrl.remove);
+router.patch('/:id/favorite', basicAuth, ctrl.toggleFavorite);
+router.patch('/:id/photo', basicAuth, upload.single('photo'), ctrl.updatePhoto);
 
 export default router;
